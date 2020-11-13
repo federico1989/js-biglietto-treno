20795c3d0167689229ea77d89cd10c0cbaeb99f7
Federico <federico.scarpati1989@gmail.com>
2020-11-13 16:39:07 +0100

    Inserimento dell'alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,20 @@ chilometri = Number(prompt("Quanti chilometri vuoi percorrere?"));
 var età;
 età  = Number(prompt("Quanti anni hai?"));
 // Prezzo del viaggio
-var prezzoBase;
-prezzo = 0.21;
+var prezzoAlKm;
+prezzoAlKm = 0.21;
 // Il prezzo del biglietto è definito in base ai km (0.21 € al km)
+var prezzo;
+prezzo = (chilometri * prezzoBase);
+// Prezzo totale
 var prezzoTotale;
-prezzoTotale = chilometri * prezzoBase
 // Applicazione dello sconto del 20% per i minorenni e del 40% per gli over 65
+if (età < 18) {
+  prezzoTotale = prezzo - (prezzo * 0.2);
+} else if (età > 65) {
+  prezzoTotale = prezzo - (prezzo * 0.4);
+} else {
+  prezzoTotale = prezzo;
+}
+// Risultato a schermo
+alert("Il prezzo del tuo biglietto è" + prezzoTotale.toFixed(2));
